afb2ddea854a3eafe672ae2861cc2f3bf77762b6
Sahlamali <41678780+Sahlamali@users.noreply.github.com>
2019-06-30 19:41:13 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-path = "Desktop\Sahla github\To be uploaded\python\FlagData.csv" 
+path = "FlagData.csv" 
 df = pd.read_csv(path)
+
 #To print the first 5 rows of the csv file
 z = df.head()
 print(z)
@@ -15,3 +16,14 @@ print("The countries in Asia are:\n",x)
 y = df[df['stripes']>2]
 y = y['name']
 print("The country with  stripes greater than 2 in their flags are",y)
+
+#To print the country name with no red color in the flag and creating a new csv file with that list
+w = df[df['red']==0]
+q = w['name']
+print("The country with no red color in the flag",q)
+w.to_csv("flagnew.csv")
+
+#To print the first 5 rows of the newly created file
+df1 = pd.read_csv("flagnew.csv")
+e = df1.head()
+print (e)
